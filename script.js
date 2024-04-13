d6978ede7b6ec3057dcb64b0975ef16ee59dc6d0
pkcez <166945001+pkcez@users.noreply.github.com>
2024-04-13 20:37:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,77 @@
-function radiansToDegrees(radians) {
-    return radians * (180 / Math.PI);
-}
+let aRot = null;
+let prevRotA = null;
+let totalRotationA = 0;
+
+let bRot = null;
+let prevRotB = null;
+let totalRotationB = 0;
 
 
 function showRot(e){
-    //console.log(e);
-    let a = Math.round(e.alpha / 90) * 90
-    let b = Math.round(e.beta / 90) * 90
-    let g = Math.round(e.gamma / 90) * 90
+    if (aRot === null){
+        aRot = e.alpha
+    }
+    
+    if (prevRotA !== null) {
+        let delta = aRot - prevRotA;
+        if (delta > 180) delta -= 360;
+        if (delta < 180) delta += 360;
+        totalRotationA += delta
+    }
+    prevRotA = e.alpha
 
-    document.getElementById("Shuv").innerHTML = Math.round(a / 90) * 90
-    document.getElementById("Flip").innerHTML = Math.round(b / 90) * 90
-    document.getElementById("KickHeel").innerHTML = Math.round(g / 90) * 90
+    if (Math.abs(totalRotationA) >= 180) {
+        spinCount++;
+        console.log('Phone has spun 180 degrees. Spin count:', spinCount);
+        
+        totalRotationA = 0;
+    }
+    ////////////////////
+    if (bRot === null){
+        bRot = e.beta
+    }
+    
+    if (prevRotB !== null) {
+        let delta = bRot - prevRotB;
+        if (delta > 180) delta -= 360;
+        if (delta < 180) delta += 360;
+        totalRotationB += delta
+    }
+    prevRotB = e.beta
+
+    if (Math.abs(totalRotationB) >= 180) {
+        spinCount++;
+        console.log('Phone has spun 180 degrees. Spin count:', spinCount);
+        
+        totalRotationB = 0;
+    }
+    ////////////////////
+    if (aRot === null){
+        aRot = e.alpha
+    }
+    
+    if (prevRotA !== null) {
+        let delta = aRot - prevRotA;
+        if (delta > 180) delta -= 360;
+        if (delta < 180) delta += 360;
+        totalRotationA += delta
+    }
+    prevRotA = e.alpha
+
+    if (Math.abs(totalRotationA) >= 180) {
+        spinCount++;
+        console.log('Phone has spun 180 degrees. Spin count:', spinCount);
+        
+        totalRotationA = 0;
+    }
+
+    let a = Math.round(e.alpha / 30) * 30
+    let b = Math.round(e.beta / 30) * 30
+    let g = Math.round(e.gamma / 30) * 30
+
+    document.getElementById("Shuv").innerHTML = a
+    document.getElementById("Flip").innerHTML = b
+    document.getElementById("KickHeel").innerHTML = g
     
 }
 async function getRot(){
